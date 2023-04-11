39357f77312c7c294d296832521c1b796814fa02
Maité <martinezf.maite@gmail.com>
2023-04-11 11:59:45 -0300

    Agrega ejemplos de fetch - get y post

--- accordion/training.js
+++ accordion/training.js
@@ -1,5 +1,5 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
