6d2c3f8fa824ba79f5c8e9145fbdac816e8f0e54
Maité <martinezf.maite@gmail.com>
2023-04-12 10:11:48 -0300

    jest

---
+++ jest/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adds -1 + 0 to equal -1', () => {
+    expect(sum(-1, 0)).toBe(-1);
+});
